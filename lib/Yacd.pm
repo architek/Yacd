3c87374f8d6fcf83371d4f2374fc84badc5a74ac
Laurent Kislaire <teebeenator@gmail.com>
2012-08-11 15:31:48 +0200

    Old lib leftovers removed

--- lib/Yacd.pm
+++ lib/Yacd.pm
@@ -5,7 +5,7 @@ use strict;
 
 =head1 NAME
 
-Ccsds - Module used to decode CCSDS TM
+Yacd - Module used to decode CCSDS TM
 
 =cut
 
@@ -21,35 +21,9 @@ This library allows decoding of CCSDS frames and packets as defined in the PSS/E
 
     use Yacd;
     use Data::Dumper;
-    
-    my $c = eval {
-    #Load Customization of protocol
-    Convert::Binary::C->new(
-        IntSize=>4,
-        ShortSize=>2,
-        Alignment=>1,
-        CompoundAlignment=>1,
-        ByteOrder=>'BigEndian',
-        UnsignedChars=>1, 
-        UnsignedBitfields=>1,
-#        OrderMembers=>1,   # requires Tie::Hash::Indexed to minimize performance decrease (min x3!)
-        ) ->parse_file('custo.h')
-    };
-    if ($@) {
-        die "The structure definition was not parsed properly:\n$@";
-    } 
-    #Prepare config to module. Callbacks are called for each frame or packet found
-    my $config={
-            coderefs_packet=>[ \&main::pkt_tm_decode, ],
-            coderefs_frame=>[\&main::tmfe_record],
-            idle_frames=>1,  #also pass idle frames to callback
-            idle_packets=>1, #also pass idle packets to callback
-            c=>$c,
-    }
+    use Convert::Binary::C;
 
-    #Launch loop. The callbacks will be triggered
-    read_frames($ARGV[0],$config);   
-
+    #User defined callbacks
     sub pkt_tm_decode{
         my ($pkt,$raw,$rec_head)=@_;
         print Dumper($pkt);
@@ -58,26 +32,54 @@ This library allows decoding of CCSDS frames and packets as defined in the PSS/E
         my ($frame,$raw,$rec_head)=@_;
         print Dumper($frame);
     }
-}
+
+    #Load Customization of protocol
+    my $c = eval {
+    Convert::Binary::C->new(
+        IntSize=>4,
+        ShortSize=>2,
+        Alignment=>1,
+        CompoundAlignment=>1,
+        ByteOrder=>'BigEndian',
+        UnsignedChars=>1,
+        UnsignedBitfields=>1,
+        #OrderMembers=>1,   # requires Tie::Hash::Indexed to minimize performance decrease (min x3!)
+        ) ->parse_file('custo.h')
+    };
+
+    if ($@) {
+        die "The structure definition was not parsed properly:\n$@";
+    }
+
+    #Prepare config to module. Callbacks are called for each frame or packet found
+    my $config={
+        coderefs_packet=>[ \&main::pkt_tm_decode, ], #callbacks for packets
+        coderefs_frame=>[\&main::tmfe_record],       #callbacks for frames
+        idle_frames=>1,                              #also pass idle frames to callback
+        idle_packets=>1,                             #also pass idle packets to callback
+        c=>$c,                                       #protocol definition
+        skip=>13000,                                 #skip n frames (until the next packet)
+        frame_nr=>20042,                             #decode n frames (until the last packet is finished)
+    };
+
+    #Launch loop on logfile
+    read_frames($ARGV[0],$config);
+
 
 =head1 AUTHOR
 
 Laurent KISLAIRE, C<< <teebeenator at gmail.com> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<teebeenator at gmail.com>
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc Ccsds
+    perldoc Yacd
 
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010 Laurent KISLAIRE.
+Copyright 2012 Laurent KISLAIRE.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
@@ -102,12 +104,12 @@ Please report any bugs or feature requests to C<teebeenator at gmail.com>
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc Ccsds
+    perldoc Yacd
 
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010 Laurent KISLAIRE.
+Copyright 2012 Laurent KISLAIRE.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
@@ -118,4 +120,4 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1;    # End of Ccsds
+1;    # End of Yacd
