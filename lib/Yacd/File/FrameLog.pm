1bd71cf08daeb82277899ea9013c85aaff24f139
Laurent Kislaire <teebeenator@gmail.com>
2012-10-04 21:09:47 +0200

    Tidy

--- lib/Yacd/File/FrameLog.pm
+++ lib/Yacd/File/FrameLog.pm
@@ -12,25 +12,29 @@ Yacd::File::FrameLog - Module to decode logfile of CCSDS TM logfile record, Cadu
 
 # BEGIN block is necessary here so that other modules can use the constants.
 use vars qw( @EXPORT_OK %EXPORT_TAGS );
+
 BEGIN {
     use Exporter qw( import );
     @EXPORT_OK   = qw/read_record frames_loop/;
     %EXPORT_TAGS = (
-        CONSTANTS => [ qw( SYNC) ],
-        ERROR_CODES => [ qw( YA_READ) ],
+        CONSTANTS   => [qw(SYNC)],
+        ERROR_CODES => [qw(YA_READ)],
 
     );
 
     # Add all the constant names and error code names to @EXPORT_OK
-    Exporter::export_ok_tags( qw( CONSTANTS ERROR_CODES) );
+    Exporter::export_ok_tags(qw(CONSTANTS ERROR_CODES));
 }
 
-use constant SYNC            => "\x1a\xcf\xfc\x1d";
-use constant YA_READ         => 0;
+use constant SYNC    => "\x1a\xcf\xfc\x1d";
+use constant YA_READ => 0;
 
 use Yacd::Packet qw/parse_packet :ERROR_CODES/;
 
-sub read_record {
+#Read a record from a file
+#Passing the configuration is needed at last for knowing the size of a record
+#
+sub read_record {    #$record($file_handle, $configuration)
     my ( $fin, $config ) = @_;
     my $c          = $config->{c};
     my $len_record = $c->sizeof('record_t');
@@ -53,19 +57,19 @@ sub read_record {
     return $raw;
 }
 
-sub frames_loop {
+sub frames_loop {    #$frame_nr($configuration,$filename)
     my ( $config, $filename ) = @_;
 
     my $c = $config->{c};
     my $res_pktdec;
     my $struct_pktdec = {};
-    my @packet_vcid   = ("") x 128;                # VC 0..127
-                                                   #Forward idle
-    my $frame_nr = 0;
-    my $skip = $config->{skip};
+    my @packet_vcid   = ("") x 128;        # VC 0..127
+                                           #Forward idle
+    my $frame_nr      = 0;
+    my $skip          = $config->{skip};
 
-    my $idle_frames   = $config->{idle_frames};
-    my $idle_packets  = $config->{idle_packets};
+    my $idle_frames  = $config->{idle_frames};
+    my $idle_packets = $config->{idle_packets};
 
     #Full packets and frame decoding
     my $full_packet = $config->{full_packet};
@@ -83,11 +87,11 @@ sub frames_loop {
         next FRAME_DECODE unless $raw = read_record( $fin, $config );
 
         #Extract record header, Frame and headers
-        my $rec_hdr      = substr $raw, 0, $c->offsetof( 'record_t', 'cadu' );
-        $raw             = substr $raw, $c->offsetof( 'record_t', 'cadu.frame' ), $c->sizeof('frame_t');
-        my $frame_hdr    = $c->unpack( 'frame_hdr_t', $raw );
+        my $rec_hdr = substr $raw, 0, $c->offsetof( 'record_t', 'cadu' );
+        $raw = substr $raw, $c->offsetof( 'record_t', 'cadu.frame' ), $c->sizeof('frame_t');
+        my $frame_hdr = $c->unpack( 'frame_hdr_t', $raw );
         my $frame_df_hdr = $c->unpack( 'frame_df_hdr_t', substr $raw, $c->sizeof('frame_hdr_t') );
-        my $fhp          = $frame_df_hdr->{fhp};
+        my $fhp = $frame_df_hdr->{fhp};
 
         #skip and such
         return $frame_nr if defined $config->{frame_nr} and $frame_nr >= $config->{frame_nr} and $fhp != 0b11111111111;
